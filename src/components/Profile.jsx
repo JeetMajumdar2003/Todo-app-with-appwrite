cf30a1d8ddeaa3b0c5ee430189d39a32c95c2bd3
Jeet Majumdar <bonimajumdar2003@gmail.com>
2024-04-27 12:44:00 +0530

    Add user profile functionality and logout feature

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { account } from '../appwrite/appwriteConfig'
+import { TodoForm, TodoList } from './index'
 
 function Profile() {
+    const navigate = useNavigate();
+    const [userDetails, setUserDetails] = useState();
+
+    useEffect(() => {
+        try {
+            const user = account.get();
+            user.then(
+                (response) => {
+                    setUserDetails(response);
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: Profile :: error - ", error);
+        }
+    }, [])
+
+    const handleLogout = async () => {
+        try {
+            await account.deleteSession('current');
+            navigate('/');
+        } catch (error) {
+            console.log("Appwrite Error :: handleLogout :: error - ", error);
+
+        }
+    }
+
+
     return (
-        <div>Profile</div>
+        <>
+            {userDetails ? (
+                <>
+                    <div className=" bg-slate-400 min-h-min mx-auto shadow-md flex justify-between text-right py-3 px-3">
+                        <div>
+                            <p className="text-lg pt-1">Hello <span className='font-serif'>{userDetails.name}</span></p>
+                        </div>
+                        <div>
+                            <button
+                                className="mx-2 p-2 cursor-pointer text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-md"
+                                onClick={handleLogout}
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                    {/* TODO FORM */}
+                    <TodoForm />
+                    {/* TODOS BOX */}
+                    <TodoList />
+                </>
+            ) : (
+                <p className="flex justify-center item-center py-12 sm:px-6 lg:px-8">
+                    <div className='border rounded-md p-5 shadow-lg'>
+                        Please Login To see Profile{" "}
+                        <Link to="/">
+                            <span className="mx-2 p-2 cursor-pointer text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-md">
+                                Login
+                            </span>
+                        </Link>
+                    </div>
+                </p>
+            )}
+        </>
     )
 }
 
